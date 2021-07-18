6a752165be2f3d6246bd752b634bb8d0589368ff
spring1975 <roger.spring@gmail.com>
2021-07-18 07:52:37 -0700

    jsdoc

--- is-truthy.ts
+++ is-truthy.ts
@@ -1,7 +1,18 @@
+type falseys = '' | 'false' | 'False' | 0 | '0' | false | null | undefined;
+
 /**
+ * @description
+ * A utility function to handle values generally considered to be falsey.
+ * Typically, shortcuts such as `!!val` are sufficient. But when dealing 
+ * with user input or other wider array, the double-bang method can produce
+ * undesired results.
+ * 
+ * 
+ * @param {any} [val] Value to be evaluated
+ * @param {falseys} [including] Falsey values to evaluate as truthy.
  * 
  */
-export const isTruthy = (val: any, including: any[] = [true]): boolean =>
+export const isTruthy = (val: any, including: Array<falseys|true> = [true]): boolean =>
   !['', 'false', 'False', 0, false, null, undefined]
     .filter(
       v =>
