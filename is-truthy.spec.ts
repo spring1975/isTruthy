b969a00b1147e33702b8ead1ce637f1714602d7c
spring1975 <roger.spring@gmail.com>
2022-03-14 12:01:22 -0700

    2 files modified

--- is-truthy.spec.ts
+++ is-truthy.spec.ts
@@ -1,15 +1,15 @@
-import { isTruthy } from './is-truthy';
+import { isTruthy, falseys } from './is-truthy';
 const desc = (val: any) => (typeof val == 'string' ? `'${val}'` : `${val}`);
 
 describe('isTruthy', () => {
   describe('without inclusion', () => {
-    ['foo', 'true', 'True', '1', '0', 'null', 'undefined'].forEach(x =>
+    ['foo', 'true', 'True', '1', '0', 'null', 'undefined'].forEach((x) =>
       it(`should return ${desc(x)} to be true`, () => {
         expect(isTruthy(x)).toBeTrue();
       })
     );
 
-    ['false', 'False', false, 0, null, undefined].forEach(x =>
+    ['false', 'False', false, 0, null, undefined].forEach((x) =>
       it(`should return ${desc(x)} to be false`, () => {
         expect(isTruthy(x)).toBeFalse();
       })
@@ -17,7 +17,8 @@ describe('isTruthy', () => {
   });
 
   describe('with inclusion', () => {
-    ['false', 'False', false, 0, null, undefined].forEach(x =>
+    const testValues: falseys[] = ['false', 'False', false, 0, null, undefined];
+    testValues.forEach((x) =>
       it(`including ${desc(x)} should return ${desc(x)} to be true`, () => {
         expect(isTruthy(x, [x])).toBeTrue();
       })
